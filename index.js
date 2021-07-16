ea1a150ee1d15a8d4aa2ab9d07226d311cef71d3
potatoman16 <virani.imran@icloud.com>
2021-07-15 20:13:29 -0400

    Fixed functionality and made it look a littler nicer

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-document.getElementById("calcButton").onclick = function() {calculate()};
-
 function calculate() {
-    var principle = document.getElementById("principle"); 
-    var interestRate = document.getElementById("interest-rate"); 
-    var compoundingPeriods = document.getElementById("compounding-periods");
-    var strCompoundingPeriods = compoundingPeriods.options[compoundingPeriods.selectedIndex].value;
+    var amount = 0;
+    var principle = document.getElementById("principle").innerHTML; 
+    var interestRate = document.getElementById("interest-rate").innerHTML / 100; 
+    var years = document.getElementById("num-years").innerHTML;
+    var compoundingPeriods = document.getElementById("compounding-periods").innerHTML;
+    var multiplier = 1;
+    var strCompoundingPeriods = compoundingPeriods.options[compoundingPeriods.selectedIndex].text;
+    if (strCompoundingPeriods == "Yearly"){
+        multiplier = 1;
+    }
+    else if(strCompoundingPeriods == "Quarterly"){
+        multiplier = 4;
+    }
+    else if(strCompoundingPeriods == "Monthly"){
+        multiplier = 12;   
+    }
+    else if(strCompoundingPeriods == "Weekly"){
+        multiplier = 52;
+    }
+    else{
+        multiplier = 365;
+    }
     
+    amount = principle * (1+interestRate);
+    console.log(principle);
+    console.log(interestRate);
+    console.log("Hello");
+    document.getElementById("amount").innerHTML = "Amount: $" + amount;
 }
